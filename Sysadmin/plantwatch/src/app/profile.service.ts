da150cef1c8a931308c21c367057796ed779a7ca
nishad.2019545 <nishad.2019545@iit.ac.lk>
2021-03-13 16:27:59 +0530

    Removed serverless offline ssm

--- Sysadmin/plantwatch/src/app/profile.service.ts
+++ Sysadmin/plantwatch/src/app/profile.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from './../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +13,14 @@ export class ProfileService {
 
   uploadTest(uploadData: FormData){
     console.log(uploadData);
-    this.http.post('http://localhost:3000/dev/v1/plantprofiles', uploadData)
+    this.http.post(`${environment.baseGateway}/v1/plantprofiles`, uploadData)
+    .subscribe(d => {
+      console.log(d);
+    });
+  }
+
+  createPlantProfile(plantData: FormData){
+    this.http.post(`${environment.baseGateway}/v1/plantprofiles`, plantData)
     .subscribe(d => {
       console.log(d);
     });
