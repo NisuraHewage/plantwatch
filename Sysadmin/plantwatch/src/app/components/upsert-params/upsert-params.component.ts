48eade290de29d97f2c863cd976348aa13834353
nishad.2019545 <nishad.2019545@iit.ac.lk>
2021-03-19 11:32:36 +0530

    Get parameters

--- Sysadmin/plantwatch/src/app/components/upsert-params/upsert-params.component.ts
+++ Sysadmin/plantwatch/src/app/components/upsert-params/upsert-params.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProfileService } from 'src/app/profile.service';
 
 @Component({
   selector: 'app-upsert-params',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpsertParamsComponent implements OnInit {
 
-  constructor() { }
+  parameters: any;
+  selectedParameterType: string = "";
+
+  constructor(private profileService: ProfileService, private router: Router, private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.parameters = [];
+    this._activatedRoute.queryParams.subscribe(
+      params =>{
+        console.log('queryParams', params['profileId']);
+        this.profileService.getParametersByProfileId(params['profileId']).subscribe((r: any) => {
+          this.parameters = r.result[0];
+          console.log(this.parameters)
+        });
+      });
   }
 
+  selectParamType(event: any){
+    this.selectParamType = event.target.value;
+  }
+
+  addParameter(){
+
+  }
+
+  save(){
+
+  }
 }
