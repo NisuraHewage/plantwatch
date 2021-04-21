840320fa111fee32bc3460e75c801c49a2c6d2bb
nishad.2019545 <nishad.2019545@iit.ac.lk>
2021-04-21 23:48:56 +0530

    Rewrote parse for multipart

--- Services/src/functions/predictDisease.js
+++ Services/src/functions/predictDisease.js
@@ -2,7 +2,7 @@
 
 const https = require('https');
 var AWS = require("aws-sdk");
-const { parse } = require('aws-multipart-parser');
+/* const { parse } = require('aws-multipart-parser'); */
 const { v4: uuidv4 } = require('uuid');
 
 const request = require('request');
@@ -158,6 +158,46 @@ try{
 }
 
 }
+
+Object.defineProperty(exports, "__esModule", { value: true });
+function getValueIgnoringKeyCase(object, key) {
+    var foundKey = Object.keys(object)
+        .find(function (currentKey) { return currentKey.toLocaleLowerCase() === key.toLowerCase(); });
+    return object[foundKey];
+}
+function getBoundary(event) {
+    return getValueIgnoringKeyCase(event.headers, 'Content-Type').split('=')[1];
+}
+function getBody(event) {
+    if (event.isBase64Encoded) {
+        return Buffer.from(event.body, 'base64').toString('binary');
+    }
+    return event.body;
+}
+const parse = function (event, spotText) {
+    var boundary = getBoundary(event);
+    console.log(boundary);
+    var result = {};
+    var bodys = getBody(event);
+      bodys.split(boundary)
+        .forEach(function (item) {
+        if (/filename=".+"/g.test(item)) {
+            result[item.match(/name=".+";/g)[0].slice(6, -2)] = {
+                type: 'file',
+                filename: item.match(/filename=".+"/g)[0].slice(10, -1),
+                contentType: item.match(/content-type:\s.+/g)[0].slice(14),
+                content: spotText ? Buffer.from(item.slice(item.search(/content-type:\s.+/g) + item.match(/content-type:\s.+/g)[0].length + 4, -4), 'binary') :
+                    item.slice(item.search(/content-type:\s.+/g) + item.match(/content-type:\s.+/g)[0].length + 4, -4),
+            };
+        }
+        else if (/name=".+"/g.test(item)) {
+            console.log("name matches")
+            result[item.match(/name=".+"/g)[0].slice(6, -1)] = item.slice(item.search(/name=".+"/g) + item.match(/name=".+"/g)[0].length + 4, -4);
+        }
+    });
+    return result;
+};
+
 
 // image with multi-part form data to identify
 // user id, plant id
